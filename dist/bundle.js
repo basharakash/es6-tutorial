b94caf1efd8a802c08640ec54f03697ad62a1ef2
basharakash <113160991+basharakash@users.noreply.github.com>
2022-09-09 11:02:04 +0600

    Initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nTypeError: C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\src\\\\index.js: Cannot read properties of null (reading 'bindings')\\n    at Scope.moveBindingTo (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\scope\\\\index.js:1042:13)\\n    at BlockScoping.updateScopeInfo (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\babel-plugin-transform-es2015-block-scoping\\\\lib\\\\index.js:364:17)\\n    at BlockScoping.run (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\babel-plugin-transform-es2015-block-scoping\\\\lib\\\\index.js:330:12)\\n    at PluginPass.BlockStatementSwitchStatementProgram (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\babel-plugin-transform-es2015-block-scoping\\\\lib\\\\index.js:70:24)\\n    at newFn (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\visitors.js:181:21)\\n    at NodePath._call (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\path\\\\context.js:53:20)\\n    at NodePath.call (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\path\\\\context.js:40:17)\\n    at NodePath.visit (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\path\\\\context.js:100:31)\\n    at TraversalContext.visitQueue (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\context.js:105:16)\\n    at TraversalContext.visitSingle (C:\\\\Users\\\\user\\\\OneDrive\\\\Desktop\\\\es6-tutorial\\\\node_modules\\\\@babel\\\\traverse\\\\lib\\\\context.js:79:19)\");\n\n//# sourceURL=webpack://es6-tutorial/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
